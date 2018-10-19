a7566ecab799367d51170df62d71f81c0afd1d4f
Nathan Shearer <nshearer@ewu.edu>
2018-10-19 15:50:33 -0700

    Added template for base page.

--- src/start_devlog_server.py
+++ src/start_devlog_server.py
@@ -1,10 +1,11 @@
 import os
 
 from devhttp import DevelopmentHttpServer
+from devhttp import render_jinja
 
 
-def test_index(request, server, assets):
-    return "<html><body><h1>Success!</h1></body></html>"
+def index_view(request, server, assets):
+    return render_jinja(assets, 'index.j2.html')
 
 
 if __name__ == '__main__':
@@ -14,9 +15,18 @@ if __name__ == '__main__':
     project = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     assets = os.path.join(project, 'assets')
 
-    server.add_multiple_static(
-        '/',
-        os.path.join(project, 'lib', 'startbootstrap-sb-admin'),
-        lambda p: os.path.basename(p) not in ('LICENSE', 'gulpfile.js'))
+    for bootstrap_dir in ('css', 'js', 'vendor'):
+        server.add_multiple_static(
+            bootstrap_dir,
+            os.path.join(project, 'lib', 'startbootstrap-sb-admin', bootstrap_dir))
 
+    server.add_static('favicon.ico', os.path.join(assets, 'favicon.ico'))
+
+    server.add_asset('base.j2.html', os.path.join(assets, 'base.j2.html'))
+    server.add_asset('index.j2.html', os.path.join(assets, 'index.j2.html'))
+    server.add_dynamic('index.html', index_view)
+
+    server.redirect('', 'index.html')
+
+    print("Serving http://127.0.0.1:8080")
     server.serve_forever('127.0.0.1', 8080)
